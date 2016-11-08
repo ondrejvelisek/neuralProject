dbcf905010075f8e274b55b1900e0e9014b7e593
Simon Sudora <simon.sudora@gmail.com>
2016-11-08 10:47:00 +0100

    create csv reader

--- src/main/java/cz/muni/fi/neural/DataReader.java
+++ src/main/java/cz/muni/fi/neural/DataReader.java
@@ -1,7 +1,50 @@
 package cz.muni.fi.neural;
 
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by Simon on 06.11.2016.
  */
 public class DataReader {
+    private CSVReader reader;
+
+    public DataReader(){
+
+    }
+    public void setFile(String fileName) throws FileNotFoundException {
+        reader = new CSVReader(new FileReader(fileName));
+    }
+
+    public ArrayList<ArrayList<Double>> csvToMatrix() throws IOException {
+        String [] nextLine;
+        ArrayList<ArrayList<Double>> matrix = new ArrayList<ArrayList<Double>>();
+        while ((nextLine = reader.readNext()) != null) {
+            ArrayList<Double> exampleValues = new ArrayList<>();
+            for(String value : nextLine){
+                double doubleValue;
+                try {
+                    doubleValue = Double.parseDouble(value);
+                }
+                catch(NumberFormatException e)
+                {
+                    break;
+                }
+                exampleValues.add(doubleValue);
+            }
+            matrix.add(exampleValues);
+        }
+        return matrix;
+    }
+
+
+
+
+
+
 }
--- src/main/java/cz/muni/fi/neural/Main.java
+++ src/main/java/cz/muni/fi/neural/Main.java
@@ -1,80 +1,32 @@
 package cz.muni.fi.neural;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        List<Double> input0 = Arrays.asList(0.00, 0.01);
-        List<Double> input1 = Arrays.asList(0.01, 0.02);
-        List<Double> input2 = Arrays.asList(0.00, 0.00);
-        List<Double> input3 = Arrays.asList(0.03, 0.02);
+    public static void main(String[] args) throws IOException {
 
         System.out.println("Creating network...");
         NeuralNetwork net = new MultilayerPerceptron(2, 16, 64, 16, 1);
 
-        System.out.println("Computing sum of "+input0);
-        List<Double> output = net.computeOutput(input0);
+        DataReader dataReader = new DataReader();
+        ArrayList<ArrayList<Double>> matrix = null;
+        try {
+            dataReader.setFile("xor_dataset.csv");
+            matrix = dataReader.csvToMatrix();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println(output);
-
-        System.out.println("Learning (sum of four numbers)...");
-
-        Map<List<Double>, List<Double>> trainingSet = new HashMap<>();
-
-        trainingSet.put(Arrays.asList(0.00, 0.00), Arrays.asList(0.00));
-        trainingSet.put(Arrays.asList(0.00, 0.01), Arrays.asList(0.01));
-        trainingSet.put(Arrays.asList(0.00, 0.02), Arrays.asList(0.02));
-        trainingSet.put(Arrays.asList(0.00, 0.03), Arrays.asList(0.03));
-
-        trainingSet.put(Arrays.asList(0.01, 0.00), Arrays.asList(0.01));
-        trainingSet.put(Arrays.asList(0.01, 0.01), Arrays.asList(0.02));
-        trainingSet.put(Arrays.asList(0.01, 0.02), Arrays.asList(0.03));
-        trainingSet.put(Arrays.asList(0.01, 0.03), Arrays.asList(0.04));
-
-        trainingSet.put(Arrays.asList(0.02, 0.00), Arrays.asList(0.02));
-        trainingSet.put(Arrays.asList(0.02, 0.01), Arrays.asList(0.03));
-        trainingSet.put(Arrays.asList(0.02, 0.02), Arrays.asList(0.04));
-        trainingSet.put(Arrays.asList(0.02, 0.03), Arrays.asList(0.05));
-
-        trainingSet.put(Arrays.asList(0.03, 0.00), Arrays.asList(0.03));
-        trainingSet.put(Arrays.asList(0.03, 0.01), Arrays.asList(0.04));
-        trainingSet.put(Arrays.asList(0.03, 0.02), Arrays.asList(0.05));
-        trainingSet.put(Arrays.asList(0.03, 0.03), Arrays.asList(0.06));
-
-        trainingSet.put(Arrays.asList(0.01, 0.00), Arrays.asList(0.01));
-        trainingSet.put(Arrays.asList(0.02, 0.00), Arrays.asList(0.02));
-        trainingSet.put(Arrays.asList(0.03, 0.00), Arrays.asList(0.03));
-
-        trainingSet.put(Arrays.asList(0.00, 0.01), Arrays.asList(0.01));
-        trainingSet.put(Arrays.asList(0.02, 0.01), Arrays.asList(0.03));
-        trainingSet.put(Arrays.asList(0.03, 0.01), Arrays.asList(0.04));
-
-        trainingSet.put(Arrays.asList(0.00, 0.02), Arrays.asList(0.02));
-        trainingSet.put(Arrays.asList(0.01, 0.02), Arrays.asList(0.03));
-        trainingSet.put(Arrays.asList(0.03, 0.02), Arrays.asList(0.05));
-
-        trainingSet.put(Arrays.asList(0.00, 0.03), Arrays.asList(0.03));
-        trainingSet.put(Arrays.asList(0.01, 0.03), Arrays.asList(0.04));
-        trainingSet.put(Arrays.asList(0.02, 0.03), Arrays.asList(0.05));
-
-        net.learn(trainingSet);
-	    net.learn(trainingSet);
-	    net.learn(trainingSet);
-
-        System.out.println("Computing sum of "+input0);
-        System.out.println(net.computeOutput(input0));
-        System.out.println("Computing sum of "+input1);
-        System.out.println(net.computeOutput(input1));
-        System.out.println("Computing sum of "+input2);
-        System.out.println(net.computeOutput(input2));
-        System.out.println("Computing sum of "+input3);
-        System.out.println(net.computeOutput(input3));
-
+        for(int i=0; i < matrix.size(); i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < matrix.get(i).size(); j++) {
+                System.out.print(matrix.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
